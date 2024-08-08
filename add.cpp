0f73148cf95e43af46e06693d5cb44cb1b6258b2
aswinkaliesgit <155635545+aswinkaliesgit@users.noreply.github.com>
2024-08-08 14:55:57 +0530

    ADD operation in independent cpp pipeline

--- add.cpp
+++ add.cpp
@@ -1,121 +1,126 @@
-#include "Device.hpp"
+#include "device.hpp"
 #include <cmath>
 #include <cstdlib>
 #include <iostream>
 #include <string>
 #include <vector>
 
-#define MATRIX_SIZE 32
+#define MATRIX_SIZE 8
 #define ONE_BYTE 8
-//Function to calculate the computing units required for each input
-std::vector<int> CalculateResource(int totalLayers, int sizeA, int sizeB)
-{
-  std::vector<int> nodeVec;
 
-  float floatSizeA = static_cast<float>(sizeA);
-  float floatSizeB = static_cast<float>(sizeB);
-  float resultA = std::ceil(floatSizeA / (SIZE_PER_PE / sizeof(float)));
-  float resultB = std::ceil(floatSizeB / (SIZE_PER_PE / sizeof(float)));
-  int intResultA = static_cast<int>(resultA);
-  int intResultB = static_cast<int>(resultB);
-  nodeVec.push_back(intResultA);
-  nodeVec.push_back(2 * intResultB);
-  nodeVec.push_back(3 * intResultA);
-  return nodeVec;
-}
-//Helper function to load input data into processing elements
-void LoadDataToPeArrays(float *A, float *B, void *peArrays[],
-                        const std::vector<int> &resourceRequired)
+// Function to calculate the computing units required for each input
+std::vector<int> calculateResource(int total_layers, int size_a, int size_b)
 {
-  for (int k = 0; k < resourceRequired.size(); k++)
-  {
-    for (int l = 0; l < resourceRequired[0]; l++)
+    std::vector<int> node_vec;
+
+    float float_size_a = static_cast<float>(size_a);
+    float float_size_b = static_cast<float>(size_b);
+    float result_a = std::ceil(float_size_a / (SIZE_PER_PE / sizeof(float)));
+    float result_b = std::ceil(float_size_b / (SIZE_PER_PE / sizeof(float)));
+    int int_result_a = static_cast<int>(result_a);
+    int int_result_b = static_cast<int>(result_b);
+    node_vec.push_back(int_result_a);
+    node_vec.push_back(2 * int_result_b);
+    node_vec.push_back(3 * int_result_a);
+    return node_vec;
+}
+
+// Helper function to load input data into processing elements
+void loadDataToPeArrays(float *a, float *b, void *pe_arrays[],
+                        const std::vector<int> &resource_required)
+{
+    for (int k = 0; k < resource_required.size(); k++)
     {
-      float *currArr =
-          static_cast<float *>(peArrays[k * resourceRequired[0] + l]);
-      for (int i = 0; i < (SIZE_PER_PE / sizeof(float)); i++)
-      {
-        if (k == 0)
+        for (int l = 0; l < resource_required[0]; l++)
         {
-          currArr[i] = A[(SIZE_PER_PE / sizeof(float)) * l + i];
+            float *curr_arr =
+                static_cast<float *>(pe_arrays[k * resource_required[0] + l]);
+            for (int i = 0; i < (SIZE_PER_PE / sizeof(float)); i++)
+            {
+                if (k == 0)
+                {
+                    curr_arr[i] = a[(SIZE_PER_PE / sizeof(float)) * l + i];
+                }
+                else if (k == 1)
+                {
+                    curr_arr[i] = b[(SIZE_PER_PE / sizeof(float)) * l + i];
+                }
+            }
         }
-        else if (k == 1)
-        {
-          currArr[i] = B[(SIZE_PER_PE / sizeof(float)) * l + i];
-        }
-      }
     }
-  }
 }
 
 int main()
 {
-  //Allocating memory to the Hardware and slicing it to make many PE's
-  const int totalPe = (PE_ROWS * PE_COLUMNS);
+    // Allocating memory to the Hardware and slicing it to make many PEs
+    const int total_pe = (PE_ROWS * PE_COLUMNS);
 
-  void *peArrays[totalPe];
-  for (int i = 0; i < totalPe; i++)
-  {
-    peArrays[i] = std::malloc(SIZE_PER_PE * ONE_BYTE);
-  }
-
-  float *A = static_cast<float *>(
-      std::malloc(MATRIX_SIZE * MATRIX_SIZE * sizeof(float)));
-  float *B = static_cast<float *>(
-      std::malloc(MATRIX_SIZE * MATRIX_SIZE * sizeof(float)));
-  float *C = static_cast<float *>(
-      std::malloc(MATRIX_SIZE * MATRIX_SIZE * sizeof(float)));
-  for (int i = 0; i < MATRIX_SIZE * MATRIX_SIZE; ++i)
-  {
-    A[i] = i + 1;
-    B[i] = i + 1;
-    C[i] = 0.0f;
-  }
-
-  std::vector<int> resourceRequired = CalculateResource(
-      3, MATRIX_SIZE * MATRIX_SIZE, MATRIX_SIZE * MATRIX_SIZE);
-  //Loading the data into the Hardware
-  LoadDataToPeArrays(A, B, peArrays, resourceRequired);
- //Doing computation and taking the result out from the hardware
-  for (int k = 0; k < resourceRequired[0]; k++)
-  {
-    float *aArr = static_cast<float *>(peArrays[k]);
-    float *bArr = static_cast<float *>(peArrays[k + resourceRequired[0]]);
-    for (int i = 0; i < (SIZE_PER_PE / sizeof(float)); i++)
+    void *pe_arrays[total_pe];
+    for (int i = 0; i < total_pe; i++)
     {
-      bArr[i] = aArr[i] + bArr[i];
+        pe_arrays[i] = std::malloc(SIZE_PER_PE * ONE_BYTE);
     }
-  }
 
-  for (int l = 0; l < resourceRequired[0]; l++)
-  {
-    float *currArr = static_cast<float *>(peArrays[resourceRequired[0] + l]);
-    for (int i = 0; i < (SIZE_PER_PE / sizeof(float)); i++)
+    float *a = static_cast<float *>(
+        std::malloc(MATRIX_SIZE * MATRIX_SIZE * sizeof(float)));
+    float *b = static_cast<float *>(
+        std::malloc(MATRIX_SIZE * MATRIX_SIZE * sizeof(float)));
+    float *c = static_cast<float *>(
+        std::malloc(MATRIX_SIZE * MATRIX_SIZE * sizeof(float)));
+    for (int i = 0; i < MATRIX_SIZE * MATRIX_SIZE; ++i)
     {
-      if (((SIZE_PER_PE / sizeof(float)) * l + i) <
-          (MATRIX_SIZE * MATRIX_SIZE))
-      {
-        C[(SIZE_PER_PE / sizeof(float)) * l + i] = currArr[i];
-      }
+        a[i] = i + 1;
+        b[i] = i + 1;
+        c[i] = 0.0f;
     }
-  }
- //Printing the output
-  for (int i = 0; i < MATRIX_SIZE; ++i)
-  {
-    for (int j = 0; j < MATRIX_SIZE; ++j)
+
+    std::vector<int> resource_required = calculateResource(
+        3, MATRIX_SIZE * MATRIX_SIZE, MATRIX_SIZE * MATRIX_SIZE);
+    
+    // Loading the data into the Hardware
+    loadDataToPeArrays(a, b, pe_arrays, resource_required);
+
+    // Doing computation and taking the result out from the hardware
+    for (int k = 0; k < resource_required[0]; k++)
     {
-      std::cout << C[i * MATRIX_SIZE + j] << " ";
+        float *a_arr = static_cast<float *>(pe_arrays[k]);
+        float *b_arr = static_cast<float *>(pe_arrays[k + resource_required[0]]);
+        for (int i = 0; i < (SIZE_PER_PE / sizeof(float)); i++)
+        {
+            b_arr[i] = a_arr[i] + b_arr[i];
+        }
     }
-    std::cout << "\n";
-  }
 
-  for (int i = 0; i < totalPe; i++)
-  {
-    std::free(peArrays[i]);
-  }
-  std::free(A);
-  std::free(B);
-  std::free(C);
+    for (int l = 0; l < resource_required[0]; l++)
+    {
+        float *curr_arr = static_cast<float *>(pe_arrays[resource_required[0] + l]);
+        for (int i = 0; i < (SIZE_PER_PE / sizeof(float)); i++)
+        {
+            if (((SIZE_PER_PE / sizeof(float)) * l + i) <
+                (MATRIX_SIZE * MATRIX_SIZE))
+            {
+                c[(SIZE_PER_PE / sizeof(float)) * l + i] = curr_arr[i];
+            }
+        }
+    }
 
-  return 0;
+    // Printing the output
+    for (int i = 0; i < MATRIX_SIZE; ++i)
+    {
+        for (int j = 0; j < MATRIX_SIZE; ++j)
+        {
+            std::cout << c[i * MATRIX_SIZE + j] << " ";
+        }
+        std::cout << "\n";
+    }
+
+    for (int i = 0; i < total_pe; i++)
+    {
+        std::free(pe_arrays[i]);
+    }
+    std::free(a);
+    std::free(b);
+    std::free(c);
+
+    return 0;
 }
