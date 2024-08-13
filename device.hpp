9487c0465ea6f834497de7c898613a32505a7a48
aswinkaliesgit <155635545+aswinkaliesgit@users.noreply.github.com>
2024-08-13 12:03:51 +0530

    Update device.hpp

--- device.hpp
+++ device.hpp
@@ -1,8 +1,7 @@
 #ifndef DEVICE_H
 #define DEVICE_H
 // The device here is a grid of 512*512
-// Each PE has a size of 2048 BYTES and
-// Float data that can be stored in a PE is 512 (512 * 4 = 2048)
+// Each PE has a size of 2048 BYTES 
 
 #define PE_ROWS 512
 #define PE_COLUMNS 512
